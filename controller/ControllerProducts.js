bde0f132e6961a9e314f93415a882f4a3580a2b7
DSM2 <fatec-dsm2@academico.intra>
2025-04-14 20:16:14 -0300

    It not completed

--- controller/ControllerProducts.js
+++ controller/ControllerProducts.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.get("/produtos", (req, res) => {
     const produtos = [
-        {nome: "4 Queijos", preco: 40, categoria: "Pizza", image: "4_QUEIKOS.png"},
-        {nome: "Mortadela", preco: 35, categoria: "Pizza", image: "MORTADELA.png"},
-        {nome: "Vegetariana", preco: 42, categoria: "Pizza", image: "VEGETARIANA.png"},
-        {nome: "Fígado e pé de galinha", preco: 55, categoria: "Pizza", image: "PE_DE_GALINHA.png"},
-        {nome: "Milho", preco: 38, categoria: "Pizza", image:"MILHO.png"}
+        {nome: "4 Queijos", preco: 40, categoria: "Pizza", image: "4-queijos.png"},
+        {nome: "Mortadela", preco: 35, categoria: "Pizza", image: "mortadela.png"},
+        {nome: "Vegetariana", preco: 42, categoria: "Pizza", image: "vegetariana.png"},
+        {nome: "Fígado e pé de galinha", preco: 55, categoria: "Pizza", image: "pe-de-galinha.png"},
+        {nome: "Milho", preco: 38, categoria: "Pizza", image:"milho.png"}
     ];
 
     res.render("produtos", {
